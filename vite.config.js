98ed28658728ec48ced814cfac6d86ec9d5a153e
ZakharovAleksey <lehazakharov@yandex.ru>
2023-06-25 19:12:09 +0200

    [+] Update base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/spanish_words_frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
